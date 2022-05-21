575bbf2ab2ade24f19ff20b0818ca9c35c2f7078
OriBracha03 <ori.bra@kfar.info>
2022-05-21 20:50:28 +0300

    Game working with GUI
    Made option to play PVP or PVC

--- src/GUI.java
+++ src/GUI.java
@@ -3,7 +3,9 @@
 //import com.company.Controller.Connect4Game;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.sql.SQLOutput;
 import javax.swing.*;
 
 /** Test setting Swing's JComponents properties and appearances */
@@ -12,10 +14,13 @@ public class GUI extends JFrame {
     private final Container cp;
 
     State game = new State();
+    MinMax AI = new MinMax();
 
     // TODO: Get it from the board
     int rows;
     int columns;
+    int gameType; //PVP or PVC
+    int AiLevel;
 
     // TODO: Calculate them instead
     int windowWidth = 750;
@@ -40,42 +45,88 @@ public class GUI extends JFrame {
         int addedRow = game.isCollumFree(row10plusCol);
 
         if(addedRow != -1) {
-
-            if(count==1){
-                JButton buttonToUpdate = ((JButton)(cp.getComponent(columns * addedRow + col)));
-                buttonToUpdate.setIcon(iconYellow);
-                game.makeMove((row10plusCol+1),1);
-            }
-            else{
-                JButton buttonToUpdate = ((JButton)(cp.getComponent(columns * addedRow + col)));
-                buttonToUpdate.setIcon(iconRed);
-                game.makeMove((row10plusCol+1),-1);
-            }
-            count*=-1;
-            game.printBoard(); // remove later
-            // check for winner
-            if(game.checkGameOver()) {
-                if(game.checkForWinnerInGUI(count)) {
-                    JOptionPane.showMessageDialog(null, "You have won!");
-                }
-                else{
-                    JOptionPane.showMessageDialog(null, "Its a tie!");
-                }
-                int reply = JOptionPane.showConfirmDialog(null, "Would you like to play again?", null, JOptionPane.YES_NO_OPTION);
-                if(reply == JOptionPane.YES_OPTION) {
-                    System.out.println("Trying to play again...");
-                    game.reset();
-                    resetBoard();
+            if (gameType == 1) {
+                if (count == 1/*||count==-1*/) {
+                    JButton buttonToUpdate = ((JButton) (cp.getComponent(columns * addedRow + col)));
+                    buttonToUpdate.setIcon(iconYellow);
+                    game.makeMove((row10plusCol + 1), 1);
                 } else {
-                    System.exit(0);
+                    JButton buttonToUpdate = ((JButton) (cp.getComponent(columns * addedRow + col)));
+                    buttonToUpdate.setIcon(iconRed);
+                    game.makeMove((row10plusCol + 1), -1);
+                }
+                count *= -1;
+                game.printBoard(); // remove later
+            }
+            else {
+                if (count == 1/*||count==-1*/) {
+                    JButton buttonToUpdate = ((JButton) (cp.getComponent(columns * addedRow + col)));
+                    buttonToUpdate.setIcon(iconYellow);
+                    game.makeMove((row10plusCol + 1), 1);
+                    //cp.add(buttonToUpdate);
+
+                /*AI.miniMax(game,5,false);
+                addedRow = game.isCollumFree(AI.getCollumn());
+                buttonToUpdate = ((JButton)(cp.getComponent(columns * addedRow + col)));
+                buttonToUpdate.setIcon(iconRed);
+                game.makeMove(AI.getCollumn()+1,-1);
+                //cp.add(buttonToUpdate);
+            */
+                } else {
+                    AI.miniMax(game, AiLevel, false);
+                    System.out.print(AI.getCollumn());
+                    System.out.print(game.isCollumFree(AI.getCollumn()));
+                    System.out.println();
+                    addedRow = game.isCollumFree(AI.getCollumn());
+                    JButton buttonToUpdate = ((JButton) (cp.getComponent(columns * addedRow + AI.getCollumn())));
+                    buttonToUpdate.setIcon(iconRed);
+                    game.makeMove(AI.getCollumn() + 1, -1);
+                    //cp.add(buttonToUpdate);
+                /*JButton buttonToUpdate = ((JButton)(cp.getComponent(columns * addedRow + col)));
+                buttonToUpdate.setIcon(iconRed);
+                game.makeMove((row10plusCol+1),-1);*/
+                }
+                count *= -1;
+                game.printBoard(); // remove later
+            }
+                // check for winner
+                if (game.checkGameOver()) {
+                    if (game.checkForWinnerInGUI(count)&&gameType==-1) {
+                        if (game.getWinner() == 1) {
+                            JOptionPane.showMessageDialog(null, "You have won!");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "AI Has Won!");
+                        }
+                    }
+                    if (game.checkForWinnerInGUI(count)&&gameType==1) {
+                        if (game.getWinner() == 1) {
+                            JOptionPane.showMessageDialog(null, "Yellow has won!");
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Red has Won!");
+                        }
+                    }
+                    else if(!game.checkIfWin()){
+                        JOptionPane.showMessageDialog(null, "Its a tie!");
+                    }
+                    int reply = JOptionPane.showConfirmDialog(null, "Would you like to play again?", null, JOptionPane.YES_NO_OPTION);
+                    if (reply == JOptionPane.YES_OPTION) {
+                        System.out.println("Trying to play again...");
+                        game.reset();
+                        resetBoard();
+                        MinMax AI = new MinMax();
+                        count = 1;
+                        gameType();
+                    } else {
+                        System.exit(0);
+                    }
                 }
             }
-        } else {
+        else {
             JOptionPane.showMessageDialog(null, "Please select a valid position.");
         }
     }
 
-    public void resetBoard() {
+    public void resetBoard(){
         for(int row = 0; row < rows; row++)
             for (int col = 0; col < columns; col++)
                 ((JButton)(cp.getComponent(columns * row + col))).setIcon(iconEmpty);
@@ -107,7 +158,7 @@ public class GUI extends JFrame {
 
         cp = getContentPane();
         cp.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
-
+        gameType();
         for(int row = 0; row < rows; row++) {
             for (int col = 0; col < columns; col++) {
                 JButton button = new JButton(); // use setter to set text and icon
@@ -118,6 +169,8 @@ public class GUI extends JFrame {
 
                 button.addActionListener(actionEvent -> worldsBestUpdater(((JButton) (actionEvent.getSource()))));
                 cp.add(button);
+                //button.addActionListener(ActionEvent -> aiTurn());
+
             }
         }
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -126,5 +179,24 @@ public class GUI extends JFrame {
         setLocationRelativeTo(null); // center window on the screen
         setSize(windowWidth, windowHeight);
         setVisible(true);
+    }
+    public void gameType(){
+        int reply = JOptionPane.showConfirmDialog(null, "Would you like to play agianst the Ai?", null, JOptionPane.YES_NO_OPTION);
+        if(reply == JOptionPane.NO_OPTION) {
+            gameType = 1;
+        }
+        else {
+            gameType=-1;
+            reply = JOptionPane.showConfirmDialog(null, "Would you like the AI to be hard?", null, JOptionPane.YES_NO_OPTION);
+            if (reply == JOptionPane.YES_OPTION) {
+                AiLevel = 6;
+                reply = JOptionPane.showConfirmDialog(null, "Very Hard?", null, JOptionPane.YES_NO_OPTION);
+                if (reply == JOptionPane.YES_OPTION) {
+                    AiLevel = 7;
+                }
+            } else {
+                AiLevel = 5;
+            }
+        }
     }
 }
